831d0d82d11b4bf0f629d8a81f4b434b0fa3ef58
Kunal Joshi <kunaljoshi99@outlook.com>
2023-09-13 16:31:33 -0400

    Completed database logic

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask, request, jsonify, url_for
 
 from sqlalchemy.sql import func
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 
 import uuid
 from datetime import time, datetime
 
 from database.db import db
-from database.schema import Receipt, Item
+from database.schema import Receipt, Item, init_db
 
 
 receipts = {}
@@ -17,12 +19,23 @@ points = {}
 Setting up SQLite Database and Flask App
 '''
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+engine = create_engine(
+    "sqlite://", 
+    connect_args={"check_same_thread": False}, 
+    poolclass=StaticPool
+)
+
+
 db.init_app(app)
-db.create_all()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 
 '''
@@ -34,6 +47,41 @@ def process_receipt():
     receipt_data = request.json
 
     #validate data
+
+    receipt_id = str(uuid.uuid4())
+    retailer = receipt_data['retailer']
+    purchase_timestamp = f"{receipt_data['purchaseDate']}T{receipt_data['purchaseTime']}"
+    purchase_date = datetime.strptime(purchase_timestamp, '%Y-%m-%dT%H:%M').date()
+    purchase_time = datetime.strptime(purchase_timestamp, '%Y-%m-%dT%H:%M').time()
+    total = receipt_data['total']
+    items = receipt_data['items']
+
+    receipt = Receipt(id=receipt_id,
+                           retailer=retailer,
+                           purchase_date=purchase_date,
+                           purchase_time=purchase_time,
+                           total=total)  
+    db.session.add(receipt)
+    db.session.commit()  
+
+    for item in items:
+        item_id = str(uuid.uuid4())
+        short_description = item['shortDescription']
+        price = item['price']
+
+        item_data = Item(id=item_id,
+                        receipt_id=receipt_id,
+                        short_description=short_description, 
+                        price=price)
+        
+        db.session.add(item_data)
+        db.session.commit()
+
+    
+
+
+
+
 
     #TODO test
     return jsonify({"success": receipt_data["retailer"]})
--- database/schema.py
+++ database/schema.py
@@ -1,10 +1,10 @@
 from .db import db
 
 class Receipt(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(64), primary_key=True)
     retailer = db.Column(db.Text, nullable=False)
-    purchaseDate = db.Column(db.Date(), nullable=False)
-    purchaseTime = db.Column(db.Time(), nullable=False)
+    purchase_date = db.Column(db.Date(), nullable=False)
+    purchase_time = db.Column(db.Time(), nullable=False)
     items = db.relationship('Item', backref='receipt', lazy=True)
     total = db.Column(db.String(100), nullable=False)
 
@@ -12,10 +12,14 @@ class Receipt(db.Model):
         return f'<Receipt {self.id} for {self.retailer}>'
 
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    receipt_id = db.Column(db.Integer, db.ForeignKey('receipt.id'), nullable=False)
+    id = db.Column(db.String(64), primary_key=True)
+    receipt_id = db.Column(db.String(64), db.ForeignKey('receipt.id'), nullable=False)
     short_description = db.Column(db.Text, nullable=False)
     price = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<Item {self.id} for Receipt {self.receipt_id}>'
+
+
+def init_db():
+    db.create_all()
